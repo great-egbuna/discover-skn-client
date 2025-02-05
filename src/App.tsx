209c14fcd3d90d87d4e182f0fc0c5913de853796
Great Egbuna <egbunagreat01@gmail.com>
2025-02-05 20:32:19 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-red-500">HELLO WORLD</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
